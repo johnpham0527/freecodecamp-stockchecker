f4e9336a29464e2783411131bc5fbf0f88b312e7
John Pham <mr.john.pham@gmail.com>
2020-06-22 18:54:19 -0400

    fix link error

--- controllers/stockHandler.js
+++ controllers/stockHandler.js
@@ -2,17 +2,17 @@ const https = require('https');
 require('dotenv').config();
 const getDb = require('../db');
 
-async function stockHandler (req, res, next) {
+function stockHandler (req, res, next) {
     const stock = req.query.stock;
     const like = req.query.like;
-    var likes = 0; //initializing variable for how many times the stock was liked
-    var price = 0; //initializing variable for the price of the stock
-    let ipAddress = req.ipInfo.ip;
+    const ipAddress = req.ipInfo.ip; //obtain the IP address using middleware
+    const link = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stock}&interval=5min&apikey=${process.env.ALPHA_VANTAGE_API_KEY}`;
+    let likes = 0; //initializing variable for how many times the stock was liked
+    let price = 0; //initializing variable for the price of the stock
 
-    const link = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stock}interval=5min&apikey=process.env.ALPHA_VANTAGE_API_KEY`;
+    console.log(`Alpha Vantage key is ${process.env.ALPHA_VANTAGE_API_KEY}`);
 
     getDb.then(function(db) {
-
         const stockRequest = https.get(link, function(stockResponse) {          
             stockResponse.setEncoding('utf8');
             let rawData = '';
@@ -64,14 +64,11 @@ async function stockHandler (req, res, next) {
                         console.log(`Error inserting stock into database: ${err}`);
                         return next(err);
                     }
-                    console.log(`Inserted stock ${stock} into database successfully`);
                 })
             }
             else { //the stock exists in the database
                 likes = result.likes; //store the current value of likes from the database
-                console.log(`likes here is ${likes}`);
                 if (like) { //did the client like this stock? If so, check to see if likes needs to be increment based on the IP address
-                    console.log(`The IP array contains ${result.ip} and the number of likes is ${likes}`);
                     let ipArray = result.ip;
                     if (ipArray.indexOf(ipAddress) === -1) { //the IP address doesn't exist in the result.ip array, so we need to update likes and the IP array
                         likes++; //increment likes by one
