8fb1960154b9b2efe26e0ebd5dde6d449a20640c
John Pham <mr.john.pham@gmail.com>
2020-06-22 19:29:23 -0400

    remove unneeded imports

--- routes/api.js
+++ routes/api.js
@@ -8,15 +8,9 @@
 
 'use strict';
 
-const getDb = require('../db');
 const stockHandler = require('../controllers/stockHandler');
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 module.exports = function (app) {
-
   app.route('/api/stock-prices')
     .get(stockHandler);
 };
