41278bf7245d17882492bce33322baa04fbaa768
John Pham <mr.john.pham@gmail.com>
2020-06-22 14:07:56 -0400

    likes outputs correctly

--- controllers/stockHandler.js
+++ controllers/stockHandler.js
@@ -2,11 +2,11 @@ const https = require('https');
 require('dotenv').config();
 const getDb = require('../db');
 
-function stockHandler (req, res, next) {
+async function stockHandler (req, res, next) {
     const stock = req.query.stock;
     const like = req.query.like;
-    let likes = 0; //initializing variable for how many times the stock was liked
-    let price = 0; //initializing variable for the price of the stock
+    var likes = 0; //initializing variable for how many times the stock was liked
+    var price = 0; //initializing variable for the price of the stock
     let ipAddress = req.ipInfo.ip;
 
     let options = {
@@ -27,37 +27,33 @@ function stockHandler (req, res, next) {
     //const link = `https://repeated-alpaca.glitch.me/v1/stock/msft/quote`
     //const link =`https://www.google.com`
 
-    const stockRequest = https.get(link, function(stockResponse) {
-    // const stockRequest = https.get(link, options, function(stockResponse) {
-    //const stockRequest = https.get(`https://repeated-alpaca.glitch.com/v1/stock/${stock}/quote`, function(stockResponse) {
-    // const stockRequest = https.request(options, function (stockResponse) {
-        // console.log(`statusCode: ${res.statusCode}`);
-        //console.log(`status code is ${stockResponse.statusCode}`);
-        //console.log(`stockResponse.headers is ${stockResponse.headers}`);
-
-        stockResponse.on('data', function(data) {
-            price = data;
-            //console.log(`data is ${data}`);
-        });
-    })
-        .on('error', function(err) {
-        console.error(`Received error while requesting stock quote: ${err}`);
-        return next(err);
-    })
-
     getDb.then(function(db) {
-        let stockToFind = {
-            stock: stock
-        }
 
-        db.collection('stocks').findOne(stockToFind, function(err, result) {
+        const stockRequest = https.get(link, function(stockResponse) {
+        // const stockRequest = https.get(link, options, function(stockResponse) {
+        //const stockRequest = https.get(`https://repeated-alpaca.glitch.com/v1/stock/${stock}/quote`, function(stockResponse) {
+        // const stockRequest = https.request(options, function (stockResponse) {
+            // console.log(`statusCode: ${res.statusCode}`);
+            //console.log(`status code is ${stockResponse.statusCode}`);
+            //console.log(`stockResponse.headers is ${stockResponse.headers}`);
+
+            stockResponse.on('data', function(data) {
+                //price = data;
+                //console.log(`data is ${data}`);
+            });
+        })
+            .on('error', function(err) {
+            console.error(`Received error while requesting stock quote: ${err}`);
+            return next(err);
+        })
+
+        stockRequest.end();
+
+        db.collection('stocks').findOne({stock: stock}, function(err, result) {
             if (err) {
                 console.log(`Error finding stock in database: ${err}`);
                 return next(err);
             }
-            console.log(`The result of the database lookup is ${result}`);
-
-
 
             if (!result) { //the stock doesn't already exist in the database
 
@@ -80,9 +76,10 @@ function stockHandler (req, res, next) {
                     console.log(`Inserted stock ${stock} into database successfully`);
                 })
             }
-            else { //result was found
-                if (like) { //if like equal true rather than undefined
-                    likes = result.likes;
+            else { //the stock exists in the database
+                likes = result.likes; //store the current value of likes from the database
+                console.log(`likes here is ${likes}`);
+                if (like) { //did the client like this stock? If so, check to see if likes needs to be increment based on the IP address
                     console.log(`The IP array contains ${result.ip} and the number of likes is ${likes}`);
                     let ipArray = result.ip;
                     if (ipArray.indexOf(ipAddress) === -1) { //the IP address doesn't exist in the result.ip array, so we need to update likes and the IP array
@@ -97,18 +94,15 @@ function stockHandler (req, res, next) {
                     }
                 }
             }
+            
+            return res.json({
+                stock: stock,
+                price: price,
+                likes: likes
+            })
+            
         })
-    })
-
-    stockRequest.end();
-
-    return res.json({
-        stock: stock,
-        price: price,
-        likes: likes //this has to come from the database
-    })
-
-    //need to return an object {stock: 'goog', price: '1,000.40', likes: '2'}
+    });
 }
 
 module.exports = stockHandler;
