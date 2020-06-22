5a746abb45bddd80e961acd6e865ddb6c5605699
John Pham <mr.john.pham@gmail.com>
2020-06-22 10:07:17 -0400

    Debugging https.get

--- controllers/stockHandler.js
+++ controllers/stockHandler.js
@@ -3,33 +3,40 @@ require('dotenv').config();
 
 function stockHandler (req, res, next) {
     const stock = req.query.stock;
+    let price = 0;
 
     let options = {
         hostname: 'repeated-alpaca.glitch.me',
         //hostname: `alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stock}&interval=5min&apikey=${process.env.ALPHA_VANTAGE}`,
-        //hostname: 'google.com',
         port: 443,
-        path: '/v1/stock/' + stock + '/quote',
-        method: 'GET'
+        //path: '/v1/stock/' + stock + '/quote',
+        path: `/v1/stock/msft/quote`,
+        method: 'GET',
+        // rejectUnauthorized: false
     }
-
-    let price = 0;
 
     console.log(`req.query.stock is ${JSON.stringify(req.query.stock)}`);
 
-    const stockRequest = https.request(options, function (stockResponse) {
+    //const link = `https://repeated-alpaca.glitch.me/v1/stock/goog/quote`
+    //const link = `https://repeated-alpaca.glitch.me/v1/stock/msft/quote`
+    const link =`https://alphavantage.co`
+
+    const stockRequest = https.get(link, function(stockResponse) {
+    // const stockRequest = https.get(link, options, function(stockResponse) {
+    //const stockRequest = https.get(`https://repeated-alpaca.glitch.com/v1/stock/${stock}/quote`, function(stockResponse) {
+    // const stockRequest = https.request(options, function (stockResponse) {
         // console.log(`statusCode: ${res.statusCode}`);
         console.log(`status code is ${stockResponse.statusCode}`);
-        console.log(`stockResponse.head is ${stockResponse.head}`);
+        console.log(`stockResponse.headers is ${stockResponse.headers}`);
 
         stockResponse.on('data', function(data) {
             price = data;
+            console.log(`data is ${data}`);
         });
-    });
-
-    stockRequest.on('error', function(error) {
-        console.error(`Received error while requesting stock quote: ${error}`);
-        return next(error);
+    })
+        .on('error', function(err) {
+        console.error(`Received error while requesting stock quote: ${err}`);
+        return next(err);
     })
 
     stockRequest.end();
