0970e3c779c1c95c1df236452e1cac200ebb6ee5
John Pham <mr.john.pham@gmail.com>
2020-06-23 17:33:33 -0400

    Add link2 and rename likeBoolean

--- controllers/handleTwoStocks.js
+++ controllers/handleTwoStocks.js
@@ -7,17 +7,18 @@ function handleTwoStocks(req, res, next) {
 
     const stock1 = req.query.stock;
     const stock2 = req.query.stock2;
-    const like = req.query.like;
+    const likeBoolean = req.query.like;
     const ipAddress = req.ipInfo.ip;
     const link1 = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stock1}&interval=5min&apikey=${process.env.ALPHA_VANTAGE_API_KEY}`;
+    const link2 = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stock2}&interval=5min&apikey=${process.env.ALPHA_VANTAGE_API_KEY}`;
     const likes1 = 0; //default value for likes for stock #1
     const likes2 = 0; //default value for likes for stock #2
     const price1 = 0; //default price for likes for stock #1
     const price2 = 0; //default price for likes for stock #2
 
     getDb.then(function(db) {
-        const stockRequest = https.get(link, function(stockResponse) {
-
+        const stockRequest1 = https.get(link1, function(stockResponse) {
+            
         })
     })
 
