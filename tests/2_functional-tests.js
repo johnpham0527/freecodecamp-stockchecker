dbe024bd3849caae8af55266f3e93f6b410ae764
John Pham <mr.john.pham@gmail.com>
2020-06-23 16:53:17 -0400

    Write test for 2 stocks

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -80,8 +80,16 @@ suite('Functional Tests', function() {
         .query({stock: 'goog', stock2: 'msft'})
         .end(function(err, res) {
           assert.equal(res.status, 200);
-          
-          //done();
+          assert.isObject(res.body, 'stockData is an object');
+          assert.isArray(res.body, 'the returned object is an array');
+          assert.equal(res.body.length, 2, 'the returned array has two stock elements');
+          assert.property(res.body[0], 'stock', 'the first stock contains stock ticker string');
+          assert.property(res.body[0], 'price', 'the first stock contains decimal price in string format');
+          assert.property(res.body[0], 'rel_likes', 'the first stock contains rel_likes, which is an integer');
+          assert.property(res.body[1], 'stock', 'the second stock contains stock ticker string');
+          assert.property(res.body[1], 'price', 'the second stock contains decimal price in string format');
+          assert.property(res.body[1], 'rel_likes', 'the second stock contains rel_likes, which is an integer');
+          done();
         })
       });
       
